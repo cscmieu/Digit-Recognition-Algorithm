7ff3b69ae5ac3efc8c463372e6230d0108b9f390
ForticheMN <157831540+ForticheMN@users.noreply.github.com>
2024-05-17 15:36:27 +0200

    OUAAAIS Lé png

--- main.py
+++ main.py
@@ -1,32 +1,79 @@
 import numpy as np # type: ignore
 from sklearn.cluster import KMeans # type: ignore
+from sklearn.decomposition import PCA
+from sklearn.datasets import load_digits
+
 from ucimlrepo import fetch_ucirepo  # type: ignore
-  
+import matplotlib.pyplot as plt 
 # fetch dataset 
 optical_recognition_of_handwritten_digits = fetch_ucirepo(id=80) 
-  
+
 # data (as pandas dataframes) 
 X = optical_recognition_of_handwritten_digits.data.features 
-y = optical_recognition_of_handwritten_digits.data.targets 
+y = optical_recognition_of_handwritten_digits.data.targets
 
-# Preprocess the data
+
 data = np.concatenate((X, y.values.reshape(-1, 1)), axis=1)
 
-# Normalize the data
-data = (data - data.mean()) / data.std()
+pca = PCA(2)
+ 
+#Transform the data
+df = pca.fit_transform(data)
+ 
+df.shape
 
-# Remove the target column
-X = data[:, :-1]
+# print(X)
+# print(y.values.reshape(-1,1))
+# # Preprocess the data
+# data = np.concatenate((X, y.values.reshape(-1, 1)), axis=1)
+# print("============ data concatenate =============")
+# print(data)
+# data.shape
 
-# Create an instance of the KMeans model and fit the data
-kmeansInstance = KMeans().fit(X)
+#Initialize the class object
+kmeans = KMeans(n_clusters= 10)
+ 
+#predict the labels of clusters.
+label = kmeans.fit_predict(df)
 
-# Get the cluster labels for each data point
-labels = kmeansInstance.labels_
+centroids = kmeans.cluster_centers_
+u_labels = np.unique(label)
+ 
+#plotting the results:
+ 
+# for i in u_labels:
+    # plt.scatter(df[label == i , 0] , df[label == i , 1] , label = i)
 
-# Get the cluster centers
-centers = kmeansInstance.cluster_centers_
 
-# Print the cluster labels and centers
-print("Cluster Labels:", labels)
-print("Cluster Centers:", centers)
+# plt.scatter(centroids[:,0] , centroids[:,1] , s = 80, color = 'black')
+# plt.legend()
+# plt.show()
+
+prediction_cluster = kmeans.predict(df)
+print("===================DATA====================")
+print(data)
+
+print("=================== DF ====================")
+print(df)
+
+
+print("=================== TARGET ====================")
+print(y)
+
+
+print("=================== PREDICT ====================")
+print(prediction_cluster)
+print(prediction_cluster.shape[0])
+
+for j in range(10):
+    list_cluster = np.zeros(10)
+    for i in range(prediction_cluster.shape[0]):
+
+        if prediction_cluster[i] == j :
+            list_cluster[y.values[i]] += 1
+    plt.clf()
+    plt.title(f"histogramme du cluster n°{j}")
+    
+    plt.stairs(list_cluster,[i for i in range(11)])
+    plt.savefig(f"C:\\Users\\alexa\\Documents\\GitHub\\Digit-Recognition-Algorithm\\Cluster{j}")
+
